a76003be695dba8aeb528c8271362ae7cd9e1287
danial137 <danial79fakhrabadi@gmail.com>
2025-01-31 15:43:27 +0330

    initial commit

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,25 +1,25 @@
-// import { EmailTemplate } from '../../../components/EmailTemplate';
-import { NextResponse } from 'next/server';
-import { Resend } from 'resend';
+// // import { EmailTemplate } from '../../../components/EmailTemplate';
+// import { NextResponse } from 'next/server';
+// import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-const fromEmail = process.env.FROM_EMAIL;
+// const resend = new Resend(process.env.RESEND_API_KEY);
+// const fromEmail = process.env.FROM_EMAIL;
 
-export async function POST(req,res) {
-    try {
-        const { data, error } = await resend.emails.send({
-            from: fromEmail,
-            to: ['danial79fakhadi@gmail.com'],
-            subject: 'Hello world',
-            react: <>
-                <p>Email Body</p>
-                  </>
-        });
+// export async function POST(req,res) {
+//     try {
+//         const { data, error } = await resend.emails.send({
+//             from: fromEmail,
+//             to: ['danial79fakhadi@gmail.com'],
+//             subject: 'Hello world',
+//             react: <>
+//                 <p>Email Body</p>
+//                   </>
+//         });
 
       
 
-        return NextResponse.json(data);
-    } catch (error) {
-        return NextResponse.json({ error });
-    }
-}
+//         return NextResponse.json(data);
+//     } catch (error) {
+//         return NextResponse.json({ error });
+//     }
+// }
